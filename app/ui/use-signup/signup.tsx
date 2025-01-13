93c09d9e7a3de007a3e3f1b27b1bf62bdf8afad3
Makhi7 <mvmangxola22@gmail.com>
2025-01-13 14:44:53 +0200

    builiding signup ui component

--- app/ui/use-signup/signup.tsx
+++ app/ui/use-signup/signup.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function SignUpForm() {
   return (
-    <div>SignUpForm</div>
+    <div className="grid grid-cols-12 gap-4 py-7">
+      <div className="col-span-6">
+        background picture with logo 
+      </div>
+      <div className="col-start-8 col-span-5">
+        form side
+      </div>
+    </div>
   )
 }
