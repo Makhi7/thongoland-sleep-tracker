986f0a8e6941de64da55bfb52e9614d3a5199bec
Makhi7 <mvmangxola22@gmail.com>
2025-02-14 23:28:35 +0200

    authentication for new user in prgress

--- app/ui/splash/subscribe-to-mail-section.tsx
+++ app/ui/splash/subscribe-to-mail-section.tsx
@@ -14,10 +14,10 @@ export default function SubscribeToMailSection(){
                 >
                     Lorem Ipsum, y al seguir leyendo distintos textos del latín, 
                     descubrió la fuente indudable. Lorem Ipsum viene de las secciones 
-                    1.10.32 y 1.10.33 de "de Finnibus Bonorum et Malorum" (Los Extremos del 
+                    1.10.32 y 1.10.33 de de Finnibus Bonorum et  (Los Extremos del 
                     Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo.
                     Este libro es un tratado de teoría de éticas, muy popular durante el 
-                    Renacimiento. La primera linea del Lorem Ipsum, "Lorem ipsum dolor sit amet..", 
+                    Renacimiento. La primera linea del Lorem Ipsum, Lorem ipsum dolor sit amet..", 
                     viene de una linea en la sección 1.10.32
                 </p>
             </div>
