0a6b89656d0a4b0c0aff89993748c9830513f647
Makhi7 <mvmangxola22@gmail.com>
2025-02-17 18:09:10 +0200

    connected neon serverless with prisma as orm.authentication for new user in progress

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth"
-import { ZodError } from "zod"
+// import { ZodError } from "zod"
 import Credentials from "next-auth/providers/credentials"
-import { signInSchema } from "./lib/zod"
+// import { signInSchema } from "./lib/zod"
 // Your own logic for dealing with plaintext password strings; be careful!
-import { saltAndHashPassword } from "@/utils/password"
-import { getUserFromDb } from "@/utils/db"
+// import { saltAndHashPassword } from "@/utils/password"
+// import { getUserFromDb } from "@/utils/db"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/lib/prisma"
+import prisma  from "@/lib/prisma"
 
 
 export const { handlers, auth } = NextAuth({
@@ -19,31 +19,31 @@ export const { handlers, auth } = NextAuth({
         email: {},
         password: {},
       },
-      authorize: async (credentials) => {
-        try {
-          let user = null
+    //   authorize: async (credentials) => {
+    //     try {
+    //       let user = null
  
-          const { email, password } = await signInSchema.parseAsync(credentials)
+    //       const { email, password } = await signInSchema.parseAsync(credentials)
  
-          // logic to salt and hash password
-          const pwHash = saltAndHashPassword(password)
+    //       // logic to salt and hash password
+    //     //   const pwHash = saltAndHashPassword(password)
  
-          // logic to verify if the user exists
-          user = await getUserFromDb(email, pwHash)
+    //       // logic to verify if the user exists
+    //       user = await getUserFromDb(email, pwHash)
  
-          if (!user) {
-            throw new Error("Invalid credentials.")
-          }
+    //       if (!user) {
+    //         throw new Error("Invalid credentials.")
+    //       }
  
-          // return JSON object with the user data
-          return user
-        } catch (error) {
-          if (error instanceof ZodError) {
-            // Return `null` to indicate that the credentials are invalid
-            return null
-          }
-        }
-      },
+    //       // return JSON object with the user data
+    //       return user
+    //     } catch (error) {
+    //       if (error instanceof ZodError) {
+    //         // Return `null` to indicate that the credentials are invalid
+    //         return null
+    //       }
+    //     }
+    //   },
     }),
   ],
 })
